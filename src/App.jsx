ce14ca576d2468be0cb66ffecfb5a62e3ce00494
Lucas Cabral <lucascabral6008@gmail.com>
2022-10-17 11:50:55 -0300

    adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import Todo from "./components/Todo";
 
 
 const style = {
-  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4 `,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form:`flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button:`border p-4 ml-2 bg-purple-500 text-slate-100 `,
+  count:`text-center p-2`
 }
 
 
